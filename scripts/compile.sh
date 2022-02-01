01a32a8cbd20defd761fa5a44122cc40b879884e
Kenton Troy <kentontroy@yahoo.com>
2022-02-01 15:38:54 -0500

    Update compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,2 +1,2 @@
 javac -cp $CLASSPATH -d ../classes ../src/KnoxJDBCConnection.java
-javac -cp $CLASSPATH -d ../classes ../src/SolrQuery.java
+javac -cp $CLASSPATH -d ../classes ../src/SolrExamples.java
